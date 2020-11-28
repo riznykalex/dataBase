5457b188cb617509743204282d9889999268be8a
riznykalex <58046631+riznykalex@users.noreply.github.com>
2020-11-28 17:17:10 +0200

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM drawdb.users
+LIMIT 0, 1000
+
+-- Date: 2020-11-28 16:23
+*/
+INSERT INTO `` (`id`,`name`,`age`) VALUES (1,'Tom',21);
+INSERT INTO `` (`id`,`name`,`age`) VALUES (2,'Alex',22);
+INSERT INTO `` (`id`,`name`,`age`) VALUES (3,'Bill',20);
+INSERT INTO `` (`id`,`name`,`age`) VALUES (4,'Jane',34);
+INSERT INTO `` (`id`,`name`,`age`) VALUES (5,'John',27);
+INSERT INTO `` (`id`,`name`,`age`) VALUES (6,'Tom',29);
